d4a30ae49f704ec18c12fda5ba74f0bfba264e25
AnmarSammour <anmarsammour2000@gmail.com>
2024-04-21 21:06:43 +0300

    Splash update

--- lib/View/lock_screen.dart
+++ lib/View/lock_screen.dart
@@ -1,53 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Lockscreen extends StatelessWidget {
+  const Lockscreen({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.30, 
-            ),
-            Image.asset(
-              'assets/img/WhatsAppIcon.png',
-              width: 100.w,
-              height: 100.h,
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.40,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'from',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 12.sp,
-                  ),
-                ),
-                Text(
-                  'FACEBOOK',
-                  style: TextStyle(
-                    color: Color(0xFF02B099),
-                    fontSize: 20.sp,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.1,
-            ),
-          ],
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
