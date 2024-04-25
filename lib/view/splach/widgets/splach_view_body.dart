882eaea615f185cad82de64994c3f5cfce983983
khaled-dreat <dreatkhaled@gmail.com>
2024-04-25 23:31:55 +0300

    Update import paths for splach_view_body.dart

--- lib/view/splach/widgets/splach_view_body.dart
+++ lib/view/splach/widgets/splach_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:whatsapp/view/splach/custom_splach_foter.dart';
-import 'package:whatsapp/view/splach/custom_splach_img.dart';
+import 'package:whatsapp/view/splach/widgets/custom_splach_foter.dart';
+import 'package:whatsapp/view/splach/widgets/custom_splach_img.dart';
 
 class SplachViewBody extends StatelessWidget {
   const SplachViewBody({
