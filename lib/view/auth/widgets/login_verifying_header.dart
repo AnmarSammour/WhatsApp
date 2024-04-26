77848393c39f94e05f5c1e16fe6a1630b2e8c131
AnmarSammour <anmarsammour2000@gmail.com>
2024-04-27 01:00:13 +0300

    Refactored Login Verifying Header code

---
+++ lib/view/auth/widgets/login_verifying_header.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class LoginverifyingHeader extends StatelessWidget {
+  const LoginverifyingHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Verifying your number',
+      style: TextStyle(
+        fontSize: 16.sp,
+        color: Color(0xFF02B099),
+        fontWeight: FontWeight.bold,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+}
