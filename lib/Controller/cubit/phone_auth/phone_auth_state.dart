d1dab58b82a4e6686881138f4811cc4489729db5
AnmarSammour <anmarsammour2000@gmail.com>
2024-05-01 23:02:47 +0300

    Refactor code in Phone Auth Cubit

--- lib/Controller/cubit/phone_auth/phone_auth_state.dart
+++ lib/Controller/cubit/phone_auth/phone_auth_state.dart
@@ -1,6 +1,27 @@
 part of 'phone_auth_cubit.dart';
 
 @immutable
-sealed class PhoneAuthState {}
+abstract class PhoneAuthState {}
 
-final class PhoneAuthInitial extends PhoneAuthState {}
+class PhoneAuthInitial extends PhoneAuthState {}
+
+class Loading extends PhoneAuthState {}
+
+class ErrorOccurred extends PhoneAuthState {
+  final String errorMsg;
+
+  ErrorOccurred({required this.errorMsg});
+}
+
+// When the user submits the phone number
+class PhoneNumberSubmited extends PhoneAuthState {}
+
+// When the user verifies the OTP
+class PhoneOTPVerified extends PhoneAuthState {}
+
+class CountrySelectedState extends PhoneAuthState {
+  final LoginModel selectedCountry;
+
+  CountrySelectedState(this.selectedCountry);
+}
+
