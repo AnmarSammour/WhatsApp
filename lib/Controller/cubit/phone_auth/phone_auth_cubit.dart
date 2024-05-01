a1312844cee341bd9c9d6efee3f216fd50b35509
AnmarSammour <anmarsammour2000@gmail.com>
2024-05-01 22:10:21 +0300

    Add Phone Auth Cubit

---
+++ lib/Controller/cubit/phone_auth/phone_auth_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'phone_auth_state.dart';
+
+class PhoneAuthCubit extends Cubit<PhoneAuthState> {
+  PhoneAuthCubit() : super(PhoneAuthInitial());
+}
---
+++ lib/Controller/cubit/phone_auth/phone_auth_state.dart
@@ -0,0 +1,6 @@
+part of 'phone_auth_cubit.dart';
+
+@immutable
+sealed class PhoneAuthState {}
+
+final class PhoneAuthInitial extends PhoneAuthState {}
