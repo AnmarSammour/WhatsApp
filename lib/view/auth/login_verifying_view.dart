e0207c38a0a14c745a9ea654fbd682edf5d3aea4
AnmarSammour <anmarsammour2000@gmail.com>
2024-05-03 22:08:58 +0300

    add Login Verifying View page

---
+++ lib/view/auth/login_verifying_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoginVerifyingView extends StatelessWidget {
+  const LoginVerifyingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
