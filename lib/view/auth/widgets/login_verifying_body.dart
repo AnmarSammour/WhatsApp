3c6ef59b220c21ead56c6daee2ff85ddec60b857
AnmarSammour <anmarsammour2000@gmail.com>
2024-05-03 22:05:34 +0300

    add Login Verifying Body page

---
+++ lib/view/auth/widgets/login_verifying_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoginVerifyingBody extends StatelessWidget {
+  const LoginVerifyingBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
