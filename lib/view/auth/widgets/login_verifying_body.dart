8f49d47bcac759ff8a70d85e01718a2a2d99360a
AnmarSammour <anmarsammour2000@gmail.com>
2024-05-03 22:07:45 +0300

    Refactoring the LoginVerifyingBody page code

--- lib/view/auth/widgets/login_verifying_body.dart
+++ lib/view/auth/widgets/login_verifying_body.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'login_verifying_header.dart';
+import 'otp_text_field.dart';
+import 'phone_number_text.dart';
+import 'code_description_text.dart';
+import 'resend_code_text.dart';
 
 class LoginVerifyingBody extends StatelessWidget {
-  const LoginVerifyingBody({super.key});
+  final String countryCode;
+  final String phoneNumber;
+
+  LoginVerifyingBody({required this.countryCode, required this.phoneNumber});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: EdgeInsets.all(16.w),
+      child: Column(
+        children: [
+          SizedBox(height: MediaQuery.of(context).size.height * 0.06),
+          LoginverifyingHeader(),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+          PhoneNumberText(
+            countryCode: countryCode,
+            phoneNumber: phoneNumber,
+          ),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+          OTPTxtField(),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+          CodeDescriptionText(),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+          ResendCodeText(),
+        ],
+      ),
+    );
   }
 }
