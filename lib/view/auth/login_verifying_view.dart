1eb588e9dc23831cfa1cbd27a23e20d701a4b988
AnmarSammour <anmarsammour2000@gmail.com>
2024-05-03 22:09:28 +0300

    Refactoring the LoginVerifyingBody page code

--- lib/view/auth/login_verifying_view.dart
+++ lib/view/auth/login_verifying_view.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'widgets/login_verifying_body.dart';
 
 class LoginVerifyingView extends StatelessWidget {
-  const LoginVerifyingView({super.key});
+  final String countryCode;
+  final String phoneNumber;
+
+  LoginVerifyingView({required this.countryCode, required this.phoneNumber});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: LoginVerifyingBody(
+        countryCode: countryCode,
+        phoneNumber: phoneNumber,
+      ),
+    );
   }
 }
