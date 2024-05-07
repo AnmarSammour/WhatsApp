e925cbe22af8c48d04b96ae40a69cabd9b41b637
AnmarSammour <anmarsammour2000@gmail.com>
2024-05-08 00:31:28 +0300

    update pubspec.yaml file

---
+++ lib/view/login/widgets/phone_num_submited.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PhoneSubmitWidget extends StatelessWidget {
+  const PhoneSubmitWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
