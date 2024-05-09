6121f2908064621aabb15d9f32de7b381473297a
AnmarSammour <anmarsammour2000@gmail.com>
2024-05-09 21:08:45 +0300

    Refactor code in PhoneVerificationListener code

--- lib/view/auth/widgets/phone_verification_listener.dart
+++ lib/view/auth/widgets/phone_verification_listener.dart
@@ -1,10 +1,33 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:whatsapp/view/user_info/user_info_view.dart';
+import '../../../controller/cubit/phone_auth/phone_auth_cubit.dart';
 
 class PhoneVerificationListener extends StatelessWidget {
-  const PhoneVerificationListener({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocListener<PhoneAuthCubit, PhoneAuthState>(
+      listenWhen: (previous, current) {
+        return previous != current;
+      },
+      listener: (context, state) {
+        if (state is PhoneOTPVerified) {
+          Navigator.pop(context);
+           // Navigator.of(context).push(MaterialPageRoute(builder: (context) => UserInfoPage()));
+        }
+
+        if (state is ErrorOccurred) {
+          String errorMsg = (state).errorMsg;
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(errorMsg),
+              backgroundColor: Colors.black,
+              duration: Duration(seconds: 3),
+            ),
+          );
+        }
+      },
+      child: Container(),
+    );
   }
 }
