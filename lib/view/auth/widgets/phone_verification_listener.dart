d6a0c6afa9a29e4557960886df79b694b76e461b
AnmarSammour <anmarsammour2000@gmail.com>
2024-05-09 21:06:50 +0300

    add PhoneVerificationListener file

---
+++ lib/view/auth/widgets/phone_verification_listener.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class PhoneVerificationListener extends StatelessWidget {
+  const PhoneVerificationListener({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
