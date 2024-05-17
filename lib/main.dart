66ffbd4f464f4d847d8834b7eae3e4943ad0ad48
AnmarSammour <anmarsammour2000@gmail.com>
2024-05-17 20:39:17 +0300

    Initialize Firebase in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:whatsapp/controller/cubit/phone_auth/phone_auth_cubit.dart';
-import 'package:whatsapp/view/splach/splach_view.dart';
+import 'package:whatsapp/view/user_info/user_info_view.dart';
 
-void main() {
+import 'controller/cubit/phone_auth/phone_auth_cubit.dart';
+import 'view/login/login_view.dart';
+import 'view/splach/splach_view.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  //Initialize widget binding before configuring Firebase
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+    name: 'whatsapp-8c4dc',
+  );
   runApp(MyApp());
 }
 
@@ -14,11 +28,20 @@ class MyApp extends StatelessWidget {
     // Wrap the app with ScreenUtilInit to enable screen adaptation
     return ScreenUtilInit(
       designSize: Size(360, 640),
-      builder: (context, _) => BlocProvider(
-        create: (context) => PhoneAuthCubit(),
+      builder: (context, _) => MultiBlocProvider(
+        providers: [
+          BlocProvider<PhoneAuthCubit>(create: (context) => PhoneAuthCubit()),
+        ],
         child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: SplachView(),
+          debugShowCheckedModeBanner: false, // Disable debug banner
+          theme: ThemeData(
+            primarySwatch: Colors.blue, // Set the primary color theme
+          ),
+          home: SplachView(), //initial screen to the splash view
+          routes: {
+            '/login': (context) => LoginView(),
+            '/userinfo': (context) => UserInfo(),
+          },
         ),
       ),
     );
