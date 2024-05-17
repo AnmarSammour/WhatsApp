70d3cb34b4af91202f68ddcff1073ed4e62938e7
AnmarSammour <anmarsammour2000@gmail.com>
2024-05-18 02:08:36 +0300

    Add Chat view

---
+++ lib/view/Chat/chat_screen_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
