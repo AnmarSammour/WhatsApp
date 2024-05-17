eb248b18f2ed50ee87eb331a55ee7fd88f3d4dbb
AnmarSammour <anmarsammour2000@gmail.com>
2024-05-18 01:57:29 +0300

    Add User Model

---
+++ lib/model/user.dart
@@ -0,0 +1,13 @@
+class UserModel {
+  final String id;
+  final String name;
+  final String imageUrl;
+  final String phoneNumber;
+
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+    required this.phoneNumber,
+  });
+}
