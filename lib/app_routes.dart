9838229a5513d366d62682c993a83904085619cd
AnmarSammour <anmarsammour2000@gmail.com>
2024-05-19 19:14:12 +0300

    add AppRoutes

---
+++ lib/app_routes.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp/view/Chat/chat_screen_view.dart';
+import 'package:whatsapp/view/user_info/user_info_view.dart';
+import 'package:whatsapp/view/login/login_view.dart';
+import 'package:whatsapp/view/splach/splach_view.dart';
+
+class AppRoutes {
+  static const String splash = '/';
+  static const String login = '/login';
+  static const String userInfo = '/userinfo';
+  static const String chat = '/chat';
+
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case splash:
+        return MaterialPageRoute(builder: (_) => SplachView());
+      case login:
+        return MaterialPageRoute(builder: (_) => LoginView());
+      case userInfo:
+        return MaterialPageRoute(builder: (_) => UserInfo());
+      case chat:
+        return MaterialPageRoute(builder: (_) => ChatScreen());
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(child: Text('No route defined for ${settings.name}')),
+          ),
+        );
+    }
+  }
+}
