4c86439b6d12c85150744a49161796a465e26358
AnmarSammour <anmarsammour2000@gmail.com>
2024-05-27 15:25:19 +0300

    Rebuilding the homeview code

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'widgets/home_view_body.dart';
 
-class HomeView extends StatelessWidget {
-  const HomeView({super.key});
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: HomeViewBody(),
+    );
   }
 }
