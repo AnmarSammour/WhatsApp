aa766da4e31c5e33e881e82ca76e06afca921b65
AnmarSammour <anmarsammour2000@gmail.com>
2024-06-01 23:39:30 +0300

    Rebuild the Chat Cubit code

--- lib/Controller/cubit/chat/chat_state.dart
+++ lib/Controller/cubit/chat/chat_state.dart
@@ -1,10 +1,28 @@
 part of 'chat_cubit.dart';
 
-sealed class ChatState extends Equatable {
+abstract class ChatState extends Equatable {
   const ChatState();
+}
 
+class ChatInitial extends ChatState {
   @override
   List<Object> get props => [];
 }
 
-final class ChatInitial extends ChatState {}
+class ChatLoaded extends ChatState {
+  final List<ChatModel> chatModel;
+
+  ChatLoaded({required this.chatModel});
+
+  @override
+  List<Object> get props => [chatModel];
+}
+
+class ChatError extends ChatState {
+  final String message;
+
+  ChatError({required this.message});
+
+  @override
+  List<Object> get props => [message];
+}
