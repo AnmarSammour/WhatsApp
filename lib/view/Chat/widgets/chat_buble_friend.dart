ee746625e9f37ffc3688cb26c6d73039b09d52e0
AnmarSammour <anmarsammour2000@gmail.com>
2024-06-09 20:56:50 +0300

    Add ChatBubbleForFriend widget

---
+++ lib/view/Chat/widgets/chat_buble_friend.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChatBubbleForFriend extends StatelessWidget {
+  const ChatBubbleForFriend({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
