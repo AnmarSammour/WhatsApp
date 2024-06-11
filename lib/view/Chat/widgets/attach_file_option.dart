ff989f944021e4543543d2da97262e48f05590cd
AnmarSammour <anmarsammour2000@gmail.com>
2024-06-11 21:43:54 +0300

    Add AttachFileOption widget

---
+++ lib/view/Chat/widgets/attach_file_option.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AttachFileOption extends StatelessWidget {
+  const AttachFileOption({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
