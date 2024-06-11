9fa0b2c36b77b07fdf4f80f9d9c0ff758202fff2
AnmarSammour <anmarsammour2000@gmail.com>
2024-06-11 21:25:57 +0300

    Add ChatBubblePainter widget

---
+++ lib/view/Chat/widgets/chat_bubble_painter.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChatBubblePainter extends StatelessWidget {
+  const ChatBubblePainter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
