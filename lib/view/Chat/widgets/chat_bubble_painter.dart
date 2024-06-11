e4a7dc790e0d5fa210b41d226025d15464e14c55
AnmarSammour <anmarsammour2000@gmail.com>
2024-06-11 21:28:00 +0300

    Rebuild the ChatBubblePainter code

--- lib/view/Chat/widgets/chat_bubble_painter.dart
+++ lib/view/Chat/widgets/chat_bubble_painter.dart
@@ -1,10 +1,82 @@
 import 'package:flutter/material.dart';
 
-class ChatBubblePainter extends StatelessWidget {
-  const ChatBubblePainter({super.key});
+class ChatBubblePainter extends CustomPainter {
+  final bool isFromFriend;
+
+  ChatBubblePainter({required this.isFromFriend});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = isFromFriend ? Color(0xFFFCFEFB) : Color(0xFFE9F8DF)
+      ..style = PaintingStyle.fill;
+
+    var path = Path();
+    if (isFromFriend) {
+      path
+        ..moveTo(size.width, 10)
+        ..lineTo(size.width, size.height - 10)
+        ..arcToPoint(
+          Offset(size.width - 10, size.height),
+          radius: Radius.circular(10),
+          clockwise: false,
+        )
+        ..lineTo(10, size.height)
+        ..arcToPoint(
+          Offset(0, size.height - 10),
+          radius: Radius.circular(10),
+          clockwise: false,
+        )
+        ..lineTo(0, 20)
+        ..lineTo(-10, 0)
+        ..lineTo(0, 0)
+        ..arcToPoint(
+          Offset(0, 0),
+          radius: Radius.circular(10),
+          clockwise: false,
+        )
+        ..lineTo(size.width - 10, 0)
+        ..arcToPoint(
+          Offset(size.width, 10),
+          radius: Radius.circular(10),
+          clockwise: false,
+        )
+        ..close();
+    } else {
+      path
+        ..moveTo(0, 10)
+        ..lineTo(0, size.height - 10)
+        ..arcToPoint(
+          Offset(10, size.height),
+          radius: Radius.circular(10),
+          clockwise: false,
+        )
+        ..lineTo(size.width - 10, size.height)
+        ..arcToPoint(
+          Offset(size.width, size.height - 10),
+          radius: Radius.circular(10),
+          clockwise: false,
+        )
+        ..lineTo(size.width, 20)
+        ..lineTo(size.width + 10, 0)
+        ..lineTo(size.width, 0)
+        ..arcToPoint(
+          Offset(size.width, 0),
+          radius: Radius.circular(10),
+          clockwise: false,
+        )
+        ..lineTo(10, 0)
+        ..arcToPoint(
+          Offset(0, 10),
+          radius: Radius.circular(10),
+          clockwise: false,
+        )
+        ..close();
+    }
+
+    canvas.drawPath(path, paint);
   }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
