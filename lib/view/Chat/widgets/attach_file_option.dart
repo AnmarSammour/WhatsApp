c9970e66c2b96d422aaf01050193c7cab9c92d08
AnmarSammour <anmarsammour2000@gmail.com>
2024-06-15 23:35:13 +0300

    Refactoring AttachFileOption code

--- lib/view/Chat/widgets/attach_file_option.dart
+++ lib/view/Chat/widgets/attach_file_option.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AttachFileOption extends StatelessWidget {
-  const AttachFileOption({super.key});
+  final IconData icon;
+  final Color color;
+  final String label;
+  final VoidCallback onTap;
+
+  const AttachFileOption({
+    Key? key,
+    required this.icon,
+    required this.color,
+    required this.label,
+    required this.onTap,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return InkWell(
+      onTap: onTap,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            width: 50.w,
+            height: 50.h,
+            decoration: BoxDecoration(
+              color: Colors.white70,
+              borderRadius: BorderRadius.circular(10.r),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  spreadRadius: 2.r,
+                  blurRadius: 10.r,
+                  offset: Offset(0, 2.h),
+                ),
+              ],
+            ),
+            child: Icon(icon, color: color, size: 30.sp),
+          ),
+          SizedBox(height: screenHeight * 0.08),
+          Text(
+            label,
+            style: TextStyle(
+              fontSize: 14.sp,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
