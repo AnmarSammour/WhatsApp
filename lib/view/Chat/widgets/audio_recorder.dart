88aa52464c7fb682fdc4769a47b598b0e4f11924
AnmarSammour <anmarsammour2000@gmail.com>
2024-06-19 23:22:46 +0300

    Add AudioRecorder widget

---
+++ lib/view/Chat/widgets/audio_recorder.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AudioRecorder extends StatelessWidget {
+  const AudioRecorder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
