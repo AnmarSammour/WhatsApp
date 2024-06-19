d949970365be18e169c987948d0b51169da70975
AnmarSammour <anmarsammour2000@gmail.com>
2024-06-19 23:27:28 +0300

    Rebuilt the Emoji Picker Widget code

--- lib/view/Chat/widgets/emoji_picker.dart
+++ lib/view/Chat/widgets/emoji_picker.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 
 class EmojiPickerWidget extends StatelessWidget {
-  const EmojiPickerWidget({super.key});
+  final Function(Emoji emoji) onEmojiSelected;
+
+  const EmojiPickerWidget({
+    Key? key,
+    required this.onEmojiSelected,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height: 300.0,
+      child: EmojiPicker(
+        onEmojiSelected: (category, emoji) {
+          onEmojiSelected(emoji);
+        },
+      ),
+    );
   }
 }
