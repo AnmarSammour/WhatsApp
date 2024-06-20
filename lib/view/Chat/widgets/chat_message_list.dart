2336c23983876c2f24e6b187fa5bdbda720c36cd
AnmarSammour <anmarsammour2000@gmail.com>
2024-06-20 22:35:41 +0300

    Add ChatMessageList Widget

---
+++ lib/view/Chat/widgets/chat_message_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChatMessageList extends StatelessWidget {
+  const ChatMessageList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
