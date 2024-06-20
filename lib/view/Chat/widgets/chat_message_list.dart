f68e1b1be225ccbf77d1e4527116400cadd1458b
AnmarSammour <anmarsammour2000@gmail.com>
2024-06-20 22:36:33 +0300

    Refactoring the ChatMessageList code

--- lib/view/Chat/widgets/chat_message_list.dart
+++ lib/view/Chat/widgets/chat_message_list.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp/model/message_model.dart';
+import 'package:whatsapp/view/Chat/widgets/chat_buble.dart';
+import 'package:whatsapp/view/Chat/widgets/chat_buble_friend.dart';
 
-class ChatMessageList extends StatelessWidget {
-  const ChatMessageList({super.key});
+class ChatMessageListWidget extends StatelessWidget {
+  final List<MessageModel> messages;
+  final String senderUID;
+
+  const ChatMessageListWidget({
+    Key? key,
+    required this.messages,
+    required this.senderUID,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Expanded(
+      child: ListView.builder(
+        reverse: true,
+        itemCount: messages.length,
+        itemBuilder: (_, index) {
+          final reversedIndex = messages.length - 1 - index;
+
+          final message = messages[reversedIndex];
+
+          if (message.senderUID == senderUID)
+            return ChatBubble(message: message);
+          else
+            return ChatBubbleForFriend(message: message);
+        },
+      ),
+    );
   }
 }
