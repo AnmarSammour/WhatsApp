6725c357a93217f94a9ec97227a30134f631cac7
AnmarSammour <anmarsammour2000@gmail.com>
2024-06-20 20:54:02 +0300

    Refactoring GalleryScreen code

--- lib/view/Chat/widgets/gallery_screen.dart
+++ lib/view/Chat/widgets/gallery_screen.dart
@@ -1,10 +1,37 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class GalleryScreen extends StatelessWidget {
-  const GalleryScreen({super.key});
+class GalleryScreen extends StatefulWidget {
+  const GalleryScreen({Key? key}) : super(key: key);
+
+  @override
+  _GalleryScreenState createState() => _GalleryScreenState();
+}
+
+class _GalleryScreenState extends State<GalleryScreen> {
+  final ImagePicker _picker = ImagePicker();
+
+  Future<void> _pickImages() async {
+    final List<XFile>? images = await _picker.pickMultiImage();
+    if (images != null && images.isNotEmpty) {
+      List<File> selectedFiles = images.map((image) => File(image.path)).toList();
+      Navigator.pop(context, selectedFiles); 
+    } else {
+      Navigator.pop(context, null); 
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _pickImages(); 
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(child: CircularProgressIndicator()), 
+    );
   }
 }
