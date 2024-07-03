93a42b96d78fefc042f17e3f27a1fed60b230970
AnmarSammour <anmarsammour2000@gmail.com>
2024-07-03 22:15:11 +0300

    Add ChatItems widgets

---
+++ lib/view/Chat/widgets/chat_items.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChatItems extends StatelessWidget {
+  const ChatItems({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
