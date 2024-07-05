045ea25c8db0a31561912a8d79a45af79bd5b38b
AnmarSammour <anmarsammour2000@gmail.com>
2024-07-05 23:04:43 +0300

    Edit Status Model

--- lib/model/status.dart
+++ lib/model/status.dart
@@ -1,29 +1,46 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 
-class Status {
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:equatable/equatable.dart';
+
+class Status extends Equatable {
   final String id;
   final String userId;
   final String imageUrl;
   final DateTime timestamp;
+  final bool isText;
+  final String text;
 
-  Status({required this.id, required this.userId, required this.imageUrl, required this.timestamp});
+  Status({
+    required this.id,
+    required this.userId,
+    required this.imageUrl,
+    required this.timestamp,
+    required this.isText,
+    required this.text,
+  });
 
   factory Status.fromSnapshot(DocumentSnapshot snapshot) {
     final data = snapshot.data() as Map<String, dynamic>?;
-
     return Status(
       id: snapshot.id,
-      userId: data?['userId'] ?? '',
+      userId: data?['uid'] ?? '',
       imageUrl: data?['imageUrl'] ?? '',
       timestamp: (data?['timestamp'] as Timestamp).toDate(),
+      isText: data?['isText'] ?? false,
+      text: data?['text'] ?? '',
     );
   }
 
   Map<String, dynamic> toDocument() {
     return {
-      'userId': userId,
+      'uid': userId,
       'imageUrl': imageUrl,
       'timestamp': Timestamp.fromDate(timestamp),
+      'isText': isText,
+      'text': text,
     };
   }
+
+  @override
+  List<Object?> get props => [id, userId, imageUrl, timestamp, isText, text];
 }
