86eefd29e0a6c97662520852cb2ab01ce2c745e2
AnmarSammour <anmarsammour2000@gmail.com>
2024-07-08 23:27:39 +0300

    Refactoring StatusState code

--- lib/Controller/cubit/status/status_state.dart
+++ lib/Controller/cubit/status/status_state.dart
@@ -8,3 +8,23 @@ sealed class StatusState extends Equatable {
 }
 
 final class StatusInitial extends StatusState {}
+
+final class StatusLoading extends StatusState {}
+
+final class StatusLoaded extends StatusState {
+  final List<Status> statuses;
+
+  const StatusLoaded(this.statuses);
+
+  @override
+  List<Object> get props => [statuses];
+}
+
+final class StatusError extends StatusState {
+  final String message;
+
+  const StatusError(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
