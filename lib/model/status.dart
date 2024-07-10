3f567e2d8ee95efdf2bc93782f0eb84063a36553
AnmarSammour <anmarsammour2000@gmail.com>
2024-07-10 23:22:32 +0300

    Edit Status model code

--- lib/model/status.dart
+++ lib/model/status.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
@@ -5,18 +6,20 @@ import 'package:equatable/equatable.dart';
 class Status extends Equatable {
   final String id;
   final String userId;
-  final String imageUrl;
+  final List<String> imageUrls;
   final DateTime timestamp;
   final bool isText;
   final String text;
+  final Color? backgroundColor;
 
-  Status({
+  const Status({
     required this.id,
     required this.userId,
-    required this.imageUrl,
+    required this.imageUrls,
     required this.timestamp,
     required this.isText,
     required this.text,
+    this.backgroundColor,
   });
 
   factory Status.fromSnapshot(DocumentSnapshot snapshot) {
@@ -24,23 +27,35 @@ class Status extends Equatable {
     return Status(
       id: snapshot.id,
       userId: data?['uid'] ?? '',
-      imageUrl: data?['imageUrl'] ?? '',
+      imageUrls: List<String>.from(data?['imageUrls'] ?? []),
       timestamp: (data?['timestamp'] as Timestamp).toDate(),
       isText: data?['isText'] ?? false,
       text: data?['text'] ?? '',
+      backgroundColor: data?['backgroundColor'] != null
+          ? Color(int.parse(data?['backgroundColor'], radix: 16))
+          : null,
     );
   }
 
   Map<String, dynamic> toDocument() {
     return {
       'uid': userId,
-      'imageUrl': imageUrl,
+      'imageUrls': imageUrls,
       'timestamp': Timestamp.fromDate(timestamp),
       'isText': isText,
       'text': text,
+      'backgroundColor': backgroundColor?.value.toRadixString(16),
     };
   }
 
   @override
-  List<Object?> get props => [id, userId, imageUrl, timestamp, isText, text];
+  List<Object?> get props => [
+        id,
+        userId,
+        imageUrls,
+        timestamp,
+        isText,
+        text,
+        backgroundColor,
+      ];
 }
