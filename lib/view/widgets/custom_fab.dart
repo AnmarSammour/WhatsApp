73a0021ee54b33248bc42f0803a20afafd48d655
AnmarSammour <anmarsammour2000@gmail.com>
2024-07-11 17:32:26 +0300

    Edit CustomFAB code

--- lib/view/widgets/custom_fab.dart
+++ lib/view/widgets/custom_fab.dart
@@ -3,17 +3,20 @@ import 'package:flutter/material.dart';
 class CustomFAB extends StatelessWidget {
   final VoidCallback onPressed;
   final IconData icon;
+  final Object? heroTag;
 
   CustomFAB({
     required this.onPressed,
     required this.icon,
+    this.heroTag,
   });
 
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
       onPressed: onPressed,
-      backgroundColor: Color(0xFF02B099),
+      backgroundColor: const Color(0xFF02B099),
+      heroTag: heroTag,
       child: Icon(icon, color: Colors.white),
     );
   }
