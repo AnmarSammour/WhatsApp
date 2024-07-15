a77aa25cc350c58c8e5859524656ff0c431fabdb
AnmarSammour <anmarsammour2000@gmail.com>
2024-07-15 23:54:45 +0300

    Added Cubit providers to the main app function

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:whatsapp/controller/cubit/chat/chat_cubit.dart';
+import 'package:whatsapp/controller/cubit/communication/communication_cubit.dart';
+import 'package:whatsapp/controller/cubit/get_numbers/get_numbers_cubit.dart';
+import 'package:whatsapp/controller/cubit/group_chat/group_chat_cubit.dart';
 import 'package:whatsapp/controller/cubit/phone_auth/phone_auth_cubit.dart';
+import 'package:whatsapp/controller/cubit/status/status_cubit.dart';
+import 'package:whatsapp/controller/cubit/user/user_cubit.dart';
 import 'package:whatsapp/firebase_options.dart';
 import 'package:whatsapp/app_routes.dart';
 
 void main() async {
-  //Initialize widget binding before configuring Firebase
+  // Initialize widget binding before configuring Firebase
   WidgetsFlutterBinding.ensureInitialized();
 
   // Initialize Firebase
@@ -26,6 +34,24 @@ class MyApp extends StatelessWidget {
       builder: (context, _) => MultiBlocProvider(
         providers: [
           BlocProvider<PhoneAuthCubit>(create: (context) => PhoneAuthCubit()),
+          BlocProvider<UserCubit>(create: (context) => UserCubit()),
+          BlocProvider<ChatCubit>(
+              create: (context) => ChatCubit(
+                    firestore: FirebaseFirestore.instance,
+                  )),
+          BlocProvider<GetNumbersCubit>(
+            create: (context) => GetNumbersCubit(
+              firestore: FirebaseFirestore.instance,
+            ),
+          ),
+          BlocProvider<CommunicationCubit>(
+              create: (context) => CommunicationCubit(
+                  firestore: FirebaseFirestore.instance,
+                  storage: FirebaseStorage.instance)),
+          BlocProvider<StatusCubit>(create: (context) => StatusCubit()),
+          BlocProvider<GroupChatCubit>(
+              create: (context) =>
+                  GroupChatCubit(firestore: FirebaseFirestore.instance)),
         ],
         child: MaterialApp(
           debugShowCheckedModeBanner: false, // Disable debug banner
