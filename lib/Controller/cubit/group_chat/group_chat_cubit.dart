687b7461542ddb3298ed85bb50e8be2b50598160
AnmarSammour <anmarsammour2000@gmail.com>
2024-07-15 23:43:25 +0300

    Add getAllUsers to GroupChat Cubit code

--- lib/Controller/cubit/group_chat/group_chat_cubit.dart
+++ lib/Controller/cubit/group_chat/group_chat_cubit.dart
@@ -1,6 +1,7 @@
+import 'dart:async';
 import 'dart:io';
-
 import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
 import 'package:whatsapp/model/message_model.dart';
 import 'package:whatsapp/model/user.dart';
@@ -8,5 +9,27 @@ import 'package:whatsapp/model/user.dart';
 part 'group_chat_state.dart';
 
 class GroupChatCubit extends Cubit<GroupChatState> {
-  GroupChatCubit() : super(GroupChatInitial());
+  final FirebaseFirestore firestore;
+
+  GroupChatCubit({
+    required this.firestore,
+  }) : super(GroupChatInitial());
+
+  Future<void> getAllUsers() async {
+    try {
+      emit(GroupChatLoading());
+      QuerySnapshot snapshot = await firestore.collection('users').get();
+
+      List<UserModel> loadedUsers = [];
+      snapshot.docs.forEach((doc) {
+        if (doc.exists) {
+          loadedUsers.add(UserModel.fromSnapshot(doc));
+        }
+      });
+
+      emit(GroupChatUsersLoaded(users: loadedUsers));
+    } catch (error) {
+      emit(GroupChatError(errorMessage: 'Error fetching users: $error'));
+    }
+  }
 }
