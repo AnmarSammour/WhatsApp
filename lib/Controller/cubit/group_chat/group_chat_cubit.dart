4a79590fe0e70c95471b0df85a383c41a33347e3
AnmarSammour <anmarsammour2000@gmail.com>
2024-07-15 23:40:58 +0300

    Edit GroupChatState code

--- lib/Controller/cubit/group_chat/group_chat_cubit.dart
+++ lib/Controller/cubit/group_chat/group_chat_cubit.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:whatsapp/model/message_model.dart';
+import 'package:whatsapp/model/user.dart';
 
 part 'group_chat_state.dart';
 
--- lib/Controller/cubit/group_chat/group_chat_state.dart
+++ lib/Controller/cubit/group_chat/group_chat_state.dart
@@ -1,10 +1,50 @@
 part of 'group_chat_cubit.dart';
 
-sealed class GroupChatState extends Equatable {
+abstract class GroupChatState extends Equatable {
   const GroupChatState();
 
   @override
   List<Object> get props => [];
 }
 
-final class GroupChatInitial extends GroupChatState {}
+class GroupChatInitial extends GroupChatState {}
+
+class GroupChatLoading extends GroupChatState {}
+
+class GroupChatCreated extends GroupChatState {}
+
+class GroupChatImageSelected extends GroupChatState {
+  final File image;
+
+  GroupChatImageSelected({required this.image});
+
+  @override
+  List<Object> get props => [image];
+}
+
+class GroupChatLoaded extends GroupChatState {
+  final List<MessageModel> messages;
+
+  GroupChatLoaded({required this.messages});
+
+  @override
+  List<Object> get props => [messages];
+}
+
+class GroupChatError extends GroupChatState {
+  final String errorMessage;
+
+  GroupChatError({required this.errorMessage});
+
+  @override
+  List<Object> get props => [errorMessage];
+}
+
+class GroupChatUsersLoaded extends GroupChatState {
+  final List<UserModel> users;
+
+  const GroupChatUsersLoaded({required this.users});
+
+  @override
+  List<Object> get props => [users];
+}
