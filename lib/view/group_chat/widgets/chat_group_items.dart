0f4b000e0cda540eebb638dde7dee2263a996901
AnmarSammour <anmarsammour2000@gmail.com>
2024-07-23 23:18:28 +0300

    Add ChatGroupItems

---
+++ lib/view/group_chat/widgets/chat_group_items.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChatGroupItems extends StatelessWidget {
+  const ChatGroupItems({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
