5ec9a679276580974db497a5c1f9d9ab92763382
AnmarSammour <anmarsammour2000@gmail.com>
2024-07-23 23:08:06 +0300

    Refactoring ChatAppBarGroup code

--- lib/view/group_chat/widgets/chat_app_bar_group.dart
+++ lib/view/group_chat/widgets/chat_app_bar_group.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
 
-class ChatAppBarGroup extends StatelessWidget {
-  const ChatAppBarGroup({super.key});
+class ChatAppBarGroup extends StatelessWidget implements PreferredSizeWidget {
+  final String groupName;
+  final String groupImageUrl;
+
+  const ChatAppBarGroup({
+    Key? key,
+    required this.groupName,
+    required this.groupImageUrl,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AppBar(
+      leading: IconButton(
+        icon: Icon(Icons.arrow_back, color: Colors.white),
+        onPressed: () {
+          Navigator.pushNamedAndRemoveUntil(
+            context,
+            '/home',
+            (Route<dynamic> route) => false,
+          );
+        },
+      ),
+      title: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          CircleAvatar(
+            backgroundImage:
+                groupImageUrl.isNotEmpty ? NetworkImage(groupImageUrl) : null,
+            radius: 16,
+            child: groupImageUrl.isEmpty
+                ? const Icon(Icons.person, size: 30, color: Colors.white)
+                : null,
+          ),
+          const SizedBox(width: 10),
+          Text(groupName, style: const TextStyle(color: Colors.white)),
+        ],
+      ),
+      backgroundColor: const Color(0xFF02B099),
+      actionsIconTheme: const IconThemeData(color: Colors.white),
+      actions: const <Widget>[
+        Icon(Icons.videocam_outlined),
+        SizedBox(width: 10),
+        Icon(Icons.call_outlined),
+        SizedBox(width: 10),
+        Icon(Icons.more_vert),
+      ],
+    );
   }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
