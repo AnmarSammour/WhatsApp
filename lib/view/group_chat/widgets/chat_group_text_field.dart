a2f59901239ba6e9b824e0859270e1ef58e9d985
AnmarSammour <anmarsammour2000@gmail.com>
2024-07-23 23:11:07 +0300

    Add ChatGroupTextField

---
+++ lib/view/group_chat/widgets/chat_group_text_field.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChatGroupTextField extends StatelessWidget {
+  const ChatGroupTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
