db6613184e583d38ddc9cab1b0a4b9f455ab0dcf
AnmarSammour <anmarsammour2000@gmail.com>
2024-08-12 23:59:32 +0300

    Rebuild ChatGroupView code

--- lib/view/group_chat/widgets/chat_group_view.dart
+++ lib/view/group_chat/widgets/chat_group_view.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp/view/group_chat/widgets/chat_body_group.dart';
 
 class ChatGroupView extends StatelessWidget {
-  const ChatGroupView({super.key});
+  final String groupId;
+  final String groupName;
+  final List<String> memberIds;
+  final String groupImageUrl;
+
+  const ChatGroupView({
+    Key? key,
+    required this.groupId,
+    required this.groupName,
+    required this.memberIds,
+    required this.groupImageUrl,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: ChatBodyGroup(
+        groupId: groupId,
+        memberIds: memberIds,
+        groupName: groupName,
+        groupImageUrl: groupImageUrl,
+      ),
+    );
   }
 }
