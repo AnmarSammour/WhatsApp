a4194ae7f7b3d5828eed6311ccd881a9dd7b9442
AnmarSammour <anmarsammour2000@gmail.com>
2024-08-14 23:42:58 +0300

    Add contact info Classes

---
+++ lib/view/contact_info/user_status.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UserStatus extends StatelessWidget {
+  const UserStatus({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
