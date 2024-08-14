a5a43b5e4465b61d80c0ae6076081696bd04ac66
AnmarSammour <anmarsammour2000@gmail.com>
2024-08-14 23:36:57 +0300

    Add Contact Info Page

---
+++ lib/view/contact_info/contact_info.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ContactInfoPage extends StatelessWidget {
+  const ContactInfoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
