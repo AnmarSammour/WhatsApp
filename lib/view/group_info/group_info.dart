a41751e7203319f9d600b67f88c7206f4ef2be4f
AnmarSammour <anmarsammour2000@gmail.com>
2024-08-16 23:32:03 +0300

    Add Group Info Page

---
+++ lib/view/group_info/group_info.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GroupInfoPage extends StatelessWidget {
+  const GroupInfoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
