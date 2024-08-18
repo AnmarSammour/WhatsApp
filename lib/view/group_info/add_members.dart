8fb89ddf7043cb3d325178dae2b6da9e7798d004
AnmarSammour <anmarsammour2000@gmail.com>
2024-08-18 23:19:02 +0300

    Add Group Info Classes

---
+++ lib/view/group_info/add_members.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddMembers extends StatelessWidget {
+  const AddMembers({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/view/group_info/group_settings_section.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GroupSettingsSection extends StatelessWidget {
+  const GroupSettingsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
