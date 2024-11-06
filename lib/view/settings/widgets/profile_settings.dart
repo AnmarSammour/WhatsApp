693e5f5d5e17aba7b7aa14ebdf55ae7d3f1f0b52
unknown <anmarsammour2000@gmail.com>
2024-11-06 16:24:01 +0200

    Add settings view

---
+++ lib/view/settings/widgets/profile_settings.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProfileSettings extends StatelessWidget {
+  const ProfileSettings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
